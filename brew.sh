8b74c78d96cc5cf2260564b4cb82ccea2e1eada6
Fedor Nezhivoi <gyzerok@users.noreply.github.com>
2025-01-16 13:16:27 +0700

    update dotfiles

--- brew.sh
+++ brew.sh
@@ -23,7 +23,7 @@ brew install findutils
 
 # Install a modern version of Bash.
 brew install bash
-brew install bash-completion2
+brew install bash-completion@2
 
 # Switch to using brew-installed bash as default shell
 if ! grep -Fq "$(brew --prefix)/bin/bash" /etc/shells; then
@@ -31,14 +31,14 @@ if ! grep -Fq "$(brew --prefix)/bin/bash" /etc/shells; then
   chsh -s "$(brew --prefix)/bin/bash";
 fi;
 
-# Install more recent versions of some macOS tools.
-brew install wget
-
 # Install other useful binaries.
+brew install wget
+brew install telnet
 brew install nvm
 brew install git
 brew install git-lfs
 brew install scc
+brew install yt-dlp
 
 # Install nice prompt
 brew install starship
@@ -48,26 +48,24 @@ brew install starship
 # brew install ripgrep
 # brew install fd
 
-# Install apps only for macOS as I don't need them on Linux VMs.
-if [ "$(uname)" == "Darwin" ]; then
-  brew tap homebrew/cask-versions
-  brew install --cask firefox
-  brew install --cask visual-studio-code
-  brew install --cask keepingyouawake
-  brew install --cask 1password
-  brew install --cask monitorcontrol
-  brew install --cask iina
-
-  # Install GnuPG to enable PGP-signing commits.
-  # Doing it last as it might require sudo password.
-  brew install --cask gpg-suite-no-mail
-fi
+brew install --cask firefox
+brew install --cask visual-studio-code
+brew install --cask keepingyouawake
+# brew install --cask monitorcontrol
+brew install --cask betterdisplay
+brew install --cask iina
+brew install --cask wezterm
+brew install --cask docker
 
 # Add fonts tap
 brew tap homebrew/cask-fonts
 # Install Fira Code
 brew install --cask font-fira-code
 brew install --cask font-fira-code-nerd-font
+
+# Install GnuPG to enable PGP-signing commits.
+# Doing it last as it might require sudo password.
+brew install --cask gpg-suite-no-mail
 
 # Remove outdated versions from the cellar.
 brew cleanup
